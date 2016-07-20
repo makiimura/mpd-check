9e20c90e9a6e1d42f28926069474d57615e892ea
Pataridus <pataridus@Patariduss-MacBook-Air.local>
2016-07-20 14:57:52 +0700

    amended var datetimePatterns

--- mpd_test.py
+++ mpd_test.py
@@ -1,6 +1,7 @@
 import requests
 from xml.dom.minidom import parseString
 from datetime import datetime
+import re
 
 class mpd(object):
 	
@@ -10,6 +11,7 @@ class mpd(object):
 		self.audios = list()
 		self.video = None
 		self.subtitles = list()
+		self.datetimePatterns = list()
 
 	def parse(self):
 		# load mpd from url
@@ -38,12 +40,66 @@ class mpd(object):
 
 		return True
 
+	'''
 	def convertTime(self, runtime):
 			try: 
 				d = datetime.strptime(runtime, "PT%HH%MM%S.%fS")
 				return (d.hour*3600)+(d.minute*60)+(d.second)
 			except ValueError:
 				return 0
+	'''
+
+	def addDateTimePattern(self, pattern):
+		self.datetimePatterns.append(re.compile(pattern))
+
+	def convertTime(self, runtime):
+		m = None
+		for r in self.datetimePatterns:
+			m = r.match(runtime)
+			if m:
+				break
+
+		# Check if we cannot match any of our format
+		# Just return 0
+		if not m:
+			return 0
+		# Get hour/minutes/second from 'm' object by group name
+
+		# Need to check if there is a group or not. if not use 0
+		# For example:
+		# PT01H30M will don't have 'secs' group, so we will use 0 for secods
+		h = 0
+		mi = 0
+		s = 0
+		ms = 0
+		
+		if 'hour' in m.groupdict().keys():
+			h = int(m.group('hour'))
+
+		if 'minutes' in m.groupdict().keys():
+			mi = int(m.group('minutes'))
+
+		if 'secs' in m.groupdict().keys():
+			s = int(m.group('secs'))
+
+
+		return (h*3600) + (mi*60) + s
+
+
+#add pattern
+'''
+obj.addDateTimePattern("PT(?P<hour>\d+)H(?P<minutes>\d+)M(?P<secs>\d+)\.(?P<mil>\d+)S")
+obj.addDateTimePattern("PT(?P<hour>\d+)H(?P<minutes>\d+)M")
+obj.addDateTimePattern("PT(?P<hour>\d+)H")
+obj.addDateTimePattern("PT(?P<hour>\d+)H(?P<secs>\d+)\.(?P<mil>\d+)S")
+ 
+test test test
+'''
+
+
+# parsing manifest
+
+
 
 class subtitle(object):
 
